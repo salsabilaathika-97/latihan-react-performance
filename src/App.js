bd0fb7e79d106da3fbc63991b1d0921e7e981f8f
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-14 22:52:17 +0800

    add use memo for lengdata function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import React, {useState, useCallback} from "react";
+import React, {useState, useCallback, useMemo} from "react";
 const Child = React.lazy(() => import ('./components/Child'));
 const Son = React.lazy(() => import ('./components/Son'));
 
 function App() {
   const [count, setCount] = useState(0);
   const [countChild, setCountChild] = useState(0);
+  const [data, setData] = useState(['Ayato', 'Childe', 'Zhongli']);
+  
   const handleCount = () => {
     setCount(count + 1);
   }
@@ -16,6 +18,17 @@ function App() {
 
   const memoHandleAnak = useCallback(() => handleAnak(), []);
 
+  const lengthData = () => {
+    console.log("Ini fungsi lengthData");
+    return data.length;
+  };
+
+  const memoLength = useMemo(() => lengthData(), [data]);
+
+  const handleData = () => {
+    setData(['Itto', 'Kaeya', 'Diluc', 'Thoma'])
+  }
+
   return (
     <div className="App">
       <h1>Parent Component</h1>
@@ -23,6 +36,8 @@ function App() {
       <button onClick={handleCount}>Mengubah nilai</button>
       <Child handleAnak={memoHandleAnak} countChild = {countChild} />
       <Son />
+      <h1>{memoLength}</h1>
+      <button onClick={handleData}>Mengubah data</button>
     </div>
   );
 }
