50fd916b126b92b1b4b07a4b4a4cfbf8001b5d37
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-14 22:00:56 +0800

    add usecallback for handle child count

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useCallback} from "react";
 import Child from './components/Child';
 import Son from './components/Son';
 
@@ -11,14 +11,17 @@ function App() {
   }
 
   const handleAnak = () => {
-    setCountChild(countChild + 1);  
+    setCountChild((data) => data + 1);  
   }
+
+  const memoHandleAnak = useCallback(() => handleAnak(), []);
+
   return (
     <div className="App">
       <h1>Parent Component</h1>
       <h1>Menghitung : {count}</h1>
       <button onClick={handleCount}>Mengubah nilai</button>
-      <Child handleAnak={handleAnak} countChild = {countChild} />
+      <Child handleAnak={memoHandleAnak} countChild = {countChild} />
       <Son />
     </div>
   );
